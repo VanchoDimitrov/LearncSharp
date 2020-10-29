cf15d29b670dd56a03aadd0345ca882a8e78f18f
Vancho Dimitrov <vancho.dimitrov@gmail.com>
2020-10-29 10:41:08 +0100

    Add files via upload

---
+++ 13IncrementOperators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _13IncrementOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            //a++; // same as a = a + 1;
+            ++a; // same as a++
+            Console.WriteLine(a);
+
+            int x;
+            x = 20;
+            int increaseByOne = ++x; // same as x = 20 + 1
+
+            Console.WriteLine(increaseByOne);
+        }
+    }
+}
