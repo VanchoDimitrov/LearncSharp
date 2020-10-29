f5bbca2b7fe0464fc03ee45e02495e48c29222f5
Vancho Dimitrov <vancho.dimitrov@gmail.com>
2020-10-29 10:31:26 +0100

    Add files via upload

---
+++ 2Comments/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _2Comments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            /*
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            */
+            
+        }
+    }
+}
