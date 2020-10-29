a42875dcf890a51e2664d2df647bad24a9cc6ee0
Vancho Dimitrov <vancho.dimitrov@gmail.com>
2020-10-29 10:38:04 +0100

    Add files via upload

---
+++ 8Constants/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _8Constants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int a = 5;
+            //a = 10; // you can't do that because value is already assigned and it is of a type constant​
+            int b = 8;
+            int sum;
+            b = 10; // this is allowed. You can override value for a variables​
+            sum = b + a;
+            Console.WriteLine($"The SUM equals {sum}");
+        }
+    }
+}
