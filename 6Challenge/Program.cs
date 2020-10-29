57b8e38b2b31b83a376320ec92cea2f91413dedb
Vancho Dimitrov <vancho.dimitrov@gmail.com>
2020-10-29 10:36:25 +0100

    Add files via upload

---
+++ 6Challenge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _6Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Your name";
+            string car = "Ferrari";
+
+            int age = 20;
+            bool activeGym = true;
+
+            Console.Write("My name is ");
+            Console.Write(name);
+            Console.Write(" and I own one of the greatest ");
+            Console.Write(car);
+            Console.Write(" in the world. I'm ");
+            Console.Write(age+ " years old and "+ activeGym+ " I go to gym.");
+        }
+    }
+}
