d3eda0639afd8adac799a06dd23e3772c4a3e861
Vancho Dimitrov <vancho.dimitrov@gmail.com>
2020-10-29 10:38:48 +0100

    Add files via upload

---
+++ 11MultipleArithmeticOperations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _11MultipleArithmeticOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // In this case by default the compiler will do the multiplication and then will do the addition​
+            int result = 3 + 4 * 2;
+            Console.WriteLine(result);
+
+            // By using parentheses we enforce that first we do the addition and after the multiplication.
+            // Same as in mathematics​
+            result = (3 + 4) * 2;
+            Console.WriteLine(result);
+        }
+    }
+}
