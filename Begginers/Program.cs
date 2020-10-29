b2ba3adb518ec39ad335ce952fad51526bf5fdf4
Vancho Dimitrov <vancho.dimitrov@gmail.com>
2020-10-29 10:46:14 +0100

    Add files via upload

---
+++ Begginers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Begginers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // arithmetic operations with literals
+            Console.WriteLine(5 + 5); // 10
+            Console.WriteLine(10 - 5); // 5
+            Console.WriteLine(5 * 5); // 25
+            Console.WriteLine(10 / 2); // 5
+
+            Console.WriteLine(24 % 7); // 3 is the remainder
+
+            // variables
+
+            int a, b;
+            a = 5;
+            b = 5;
+
+            Console.WriteLine($"a+b {a + b}");
+            Console.WriteLine($"a-b {a - b}");
+            Console.WriteLine($"a*b {a * b}");
+            Console.WriteLine($"a/b {a / b}");
+
+
+            int sum;
+            sum = a + b * 10;
+            Console.WriteLine(sum); // 55
+
+            sum = (a + b) * 10;
+            Console.WriteLine(sum); // 100
+        }
+    }
+}
