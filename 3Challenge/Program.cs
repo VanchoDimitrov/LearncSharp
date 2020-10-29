9de8f4314d2403c9eb0b47558d08ad4c8c785c2a
Vancho Dimitrov <vancho.dimitrov@gmail.com>
2020-10-29 10:32:36 +0100

    Add files via upload

---
+++ 3Challenge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Let's print out the following
+
+            //Software Engineer: Your Name​
+            //, c# is not difficult to learn after all!​
+
+            Console.Write("Software Engineer: Vancho Dimitrov");
+            Console.Write(", c# is not difficult to learn after all!");
+
+            //Remember the methods WriteLine writes in one line, and the method Write writes in a line but the
+            //second print out will also be written in the same line thus forming a sentence.
+
+            Console.WriteLine(); // that will move the print cursor to a new line
+            Console.WriteLine("Software Engineer: Vancho Dimitrov");
+            Console.WriteLine(", c# is not difficult to learn after all!");
+        }
+    }
+}
